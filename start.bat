da177a7980b700581807427560374931bd214ef7
kushuri12 <kyoha199@gmail.com>
2026-01-10 19:07:32 +0800

    update bat

--- start.bat
+++ start.bat
@@ -1,1 +1,14 @@
-npm start
+@echo off
+TITLE AI ChatBot - Masha
+echo Checking dependencies...
+
+IF NOT EXIST "node_modules\" (
+    echo node_modules folder not found. Installing dependencies...
+    call npm install
+) ELSE (
+    echo Dependencies are already installed.
+)
+
+echo Starting the application...
+call npm start
+pause
